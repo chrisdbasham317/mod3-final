f4dcd09d2b40e1e7a0685e95a0f2b9c2bb680fc3
Chris Basham <chrisdbasham0317@gmail.com>
2019-11-06 09:25:13 -0700

    Remove commented out code from app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,42 +1,20 @@
 import React from 'react';
 import './App.css';
-// import { connect } from 'react-redux';
-// import { bindActionCreators } from 'redux';
-// import { setOrders } from '../../actions';
-// import { getOrders } from '../../apiCalls';
 import Orders from '../../containers/Orders/Orders';
 import OrderForm from '../OrderForm/OrderForm';
 
 const App = () => {
-  // componentDidMount() {
-  //   getOrders()
-  //     .then(data => this.props.setOrders(data.orders))
-  //     .catch(err => console.error('Error fetching:', err));
-  // }
-    return (
-      <main className="App">
-        <header>
-          <h1>Burrito Builder</h1>
-          <OrderForm />
-        </header>
+  return (
+    <main className="App">
+      <header>
+        <h1>Burrito Builder</h1>
+        <OrderForm />
+      </header>
         
-        <Orders />
-      </main>
-    );
-  }
-
-
-// export const mapStateToProps = ({ orders }) => ({
-//   orders
-// });
-
-// export const mapDispatchToProps = dispatch => (
-//   bindActionCreators({
-//     setOrders,
-//   }, dispatch)
-// );
-
-// export default connect(mapStateToProps, mapDispatchToProps)(App);
+      <Orders />
+    </main>
+  );
+};
 
 export default App;
 
